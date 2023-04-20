df2e86e7db931f37f416e2dcc508b89c9fabbf7b
James Watson <james.watson-2@colorado.edu>
2023-04-20 15:31:52 -0600

    AST oh god what is wrong w me

--- atoms.cpp
+++ atoms.cpp
@@ -4,6 +4,8 @@
 using std::string;
 #include <map>
 using std::map;
+#include <vector>
+using std::vector;
 
 typedef unsigned long ulong;
 
@@ -28,6 +30,21 @@ struct Var64{
 struct Context{
     Context* /*-------------*/ parent;
     map<string, Var64<double>> flotStore;
-    map<string, Var64<long>>   ntgrStore;
+    map<string, Var64<long>>   intrStore;
     map<string, Var64<ulong>>  uintStore;
+};
+
+////////// ABSTRACT SYNTAX TREE ////////////////////////////////////////////////////////////////////
+
+enum E_Expr{
+    ASSIGN,
+    LITERAL,
+    RHV,
+};
+
+struct AST_Expr{
+    E_Expr /*------*/ exp; // Expression type
+    E_Type /*------*/ typ; // Datatype it resolves to
+    vector<AST_Expr*> opr; // Operands
+    void* /*-------*/ dat; // Data payload
 };
