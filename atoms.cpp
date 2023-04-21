dab988bab60df02b80c73eced918bf09a1dcf7b9
James Watson <james.watson-2@colorado.edu>
2023-04-21 15:28:10 -0600

    THIS IS IRRATIONAL

--- atoms.cpp
+++ atoms.cpp
@@ -6,6 +6,10 @@ using std::string;
 using std::map;
 #include <vector>
 using std::vector;
+#include <sstream>
+using std::istringstream;
+#include <iostream>
+using std::noskipws, std::skipws;
 
 typedef unsigned long ulong;
 
@@ -15,13 +19,19 @@ enum E_Type{
     FLOAT,
     INTGR,
     U_INT,
+    ERROR,
+};
+
+enum E_Error{
+    OKAY,
 };
 
 template <typename T>
 struct Var64{
-    T      data;
+    T /*-*/ data;
+    E_Type  type;
+    E_Error err
     // ulong  refCount; // 2023-04-20: Not used?
-    E_Type typ;
 };
 
 
@@ -32,20 +42,76 @@ struct Context{
     map<string, Var64<double>> flotStore;
     map<string, Var64<long>>   intrStore;
     map<string, Var64<ulong>>  uintStore;
+    map<string, Var64<string>> strnStore;
 };
 
-////////// ABSTRACT SYNTAX TREE ////////////////////////////////////////////////////////////////////
+////////// LEXER ///////////////////////////////////////////////////////////////////////////////////
+// The lexer recognizes tokens
+
+///// Predicates /////
+
+bool p_float_string( string myString ) {
+    // Original Author: Bill the Lizard, https://stackoverflow.com/a/447307
+    std::istringstream iss( myString );
+    double f;
+    iss >> skipws >> f; // skipws considers leading whitespace valid
+    // Check the entire string was consumed and if either failbit or badbit is set
+    return iss.eof() && !iss.fail(); 
+}
+
+bool p_int_string( string myString ) {
+    // Original Author: Bill the Lizard, https://stackoverflow.com/a/447307
+    std::istringstream iss( myString );
+    long f;
+    iss >> skipws >> f; // skipws considers leading whitespace valid
+    // Check the entire string was consumed and if either failbit or badbit is set
+    return iss.eof() && !iss.fail(); 
+}
+
+bool p_uint_string( string myString ) {
+    // Original Author: Bill the Lizard, https://stackoverflow.com/a/447307
+    std::istringstream iss( myString );
+    ulong f;
+    iss >> skipws >> f; // skipws considers leading whitespace valid
+    // Check the entire string was consumed and if either failbit or badbit is set
+    return iss.eof() && !iss.fail(); 
+}
+
+map<string,string> RESERVED_TOKENS; // Reserved tokens ALWAYS stand on their own and are NEVER part of an identifier
+map<string,string> RESERVED_WORDS; //- Reserved words CAN stand on their own ONLY when they are NOT part of an identifier
+
+void init_reserved(){
+    // Reserved tokens ALWAYS stand on their own and are NEVER part of an identifier
+    RESERVED_TOKENS["("] = "open_parn"; // Open  paren
+    RESERVED_TOKENS[")"] = "clos_parn"; // Close paren
+    RESERVED_TOKENS[";"] = "semicolon"; // Semicolon
+    RESERVED_TOKENS["{"] = "open_crly"; // Open  curly brace
+    RESERVED_TOKENS["}"] = "clos_crly"; // Close curly brace
+    // Reserved words CAN stand on their own ONLY when they are NOT part of an identifier
+    RESERVED_WORDS["int"  ] = "integerDc"; // Integer declaration
+    RESERVED_WORDS["uint" ] = "unsignedD"; // Unsigned integer declaration
+    RESERVED_WORDS["float"] = "floatDecl"; // Float declaration
+}
+
+
+////////// PARSER //////////////////////////////////////////////////////////////////////////////////
+// The parder recognizes structures that can be converted to ASTs
+
+///// ABSTRACT SYNTAX TREE ///////////////////////
 
 enum E_Expr{
     ASSIGN,
     LITERAL,
     RHV,
     DEFINE,
+    MATH,
 };
 
 struct AST_Expr{
     E_Expr /*------*/ exp; // Expression type
     E_Type /*------*/ typ; // Datatype it resolves to
+    E_Error /*-----*/ err; // Error at this level of parsing
     vector<AST_Expr*> opr; // Operands
     void* /*-------*/ dat; // Data payload
 };
+
