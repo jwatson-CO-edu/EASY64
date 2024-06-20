77b37e5bba64032a265eef06928afe2762b4797e
James Watson <james.watson-2@colorado.edu>
2024-06-20 14:08:57 -0600

    This is fine

--- 00_math.c
+++ 00_math.c
@@ -2,6 +2,7 @@
 
 /// Imports ///
 #include <stddef.h> // NULL, etc.
+#include <ctype.h> //- Character tests, etc.
 
 /// Aliases ///
 typedef unsigned long ulong;
@@ -123,31 +124,36 @@ typedef struct{
 ////////// DATA STRUCTURES /////////////////////////////////////////////////////////////////////////
 // WARNING: ??? YAGNI ???
 
+///// Basic Elements //////////////////////////////////////////////////////
+
 typedef struct{
     // Element of a stack, Holds generic data
     void* data;
+    void* prev;
     void* next;
-}StackNode;
+}Elem;
 
-Stack* make_StackNode( void ){
-    // Return an empty `StackNode`
-    StackNode* rtnNode = (StackNode*) malloc( sizeof( StackNode ) );
-    rtnNode->data = NULL;
+Elem* make_Elem( void* data_ ){
+    // Return an empty `Elem`
+    Elem* rtnNode = (Elem*) malloc( sizeof( Elem ) );
+    rtnNode->data = data_;
+    rtnNode->prev = NULL;
     rtnNode->next = NULL;
     return rtnNode;
 }
 
-void del_StackNode( StackNode* node ){
+void del_Elem( Elem* node ){
     // Erase `node`
     free( node->data );
-    free( node->next );
     free( node );
 }
 
+///// Stack ///////////////////////////////////////////////////////////////
+
 typedef struct{
     // Generic stack
-    StackNode* top;
-    ulong /**/ len;
+    Elem* top;
+    ulong len;
 }Stack;
 
 Stack* make_Stack( void ){
@@ -160,8 +166,7 @@ Stack* make_Stack( void ){
 
 void push_Stack( Stack* stck, void* elemData ){
     // Push data onto stack
-    StackNode* nuNode = (StackNode*) malloc( sizeof( StackNode ) );
-    nuNode->data = elemData;
+    Elem* nuNode = make_Elem( elemData );
     nuNode->next = stck->top;
     stck->top   = nuNode;
     (stck->len)++;
@@ -169,25 +174,165 @@ void push_Stack( Stack* stck, void* elemData ){
 
 void* pop_Stack( Stack* stck ){
     // Pop data from stack
-    StackNode* stkTop = NULL;
+    Elem* stkTop = NULL;
     void* /**/ rtnDat = NULL;
     if( stck->len > 0 ){
         stkTop    = stck->top;
         stck->top = stkTop->next;
         rtnDat    = stkTop->data;
-        del_StackNode( stkTop );
+        free( stkTop ); // Leaves data intact @ heap
+        (stck->len)--;
     }
     return rtnDat;
 }
 
 void del_Stack( Stack* stck ){
     // Erase `stck` and all nodes contained therein
-    StackNode* stkTop = NULL;
-    while( stck->len ){  pop_Stack( stck );  }
+    Elem* stkTop = NULL;
+    void* data_i;
+    while( stck->len ){  
+        data_i = pop_Stack( stck );  
+        free( data_i );
+    }
     free( stck );
 }
 
 
+///// Queue ///////////////////////////////////////////////////////////////
+
+typedef struct{
+    // Generic stack
+    Elem* front;
+    Elem* back;
+    ulong len;
+}Queue;
+
+Queue* make_Queue( void ){
+    // Return an empty `Queue`
+    Queue* rtnQ = (Queue*) malloc( sizeof( Queue ) );
+    rtnQ->front = NULL;
+    rtnQ->back  = NULL;
+    rtnQ->len   = 0;
+    return rtnQ;
+}
+
+void push_back_Q( Queue* q, void* elemData ){
+    // Push data onto `q` back
+    Elem* nuNode = make_Elem( elemData );
+    if( (q->len) == 0 ){  
+        q->front = nuNode;    
+    }else{
+        q->back->next = nuNode;
+    }
+    q->back = nuNode;
+    (q->len)++;
+}
+
+void* pop_front_Q( Queue* q ){
+    // Pop data from `q` front
+    Elem* qFront = NULL;
+    void* rtnDat = NULL;
+    if( q->len > 0 ){
+        qFront    = q->front;
+        q->front  = qFront->next;
+        rtnDat    = qFront->data;
+        free( qFront );
+        (q->len)--;
+    }
+    return rtnDat;
+}
+
+void del_Queue( Queue* q ){
+    // Erase `q` and all nodes contained therein
+    Elem* stkTop = NULL;
+    void* data_i;
+    while( q->len ){  
+        data_i = pop_front_Q( q );  
+        free( data_i );
+    }
+    free( q );
+}
+
+///// Queue as String /////////////////////////////////////////////////////
+#define _CHUNK_LEN 128
+
+typedef struct{
+    // Wrapper for a `Queue` of character chunks
+    Queue* q;
+    ulong  len;
+}String;
+
+String* make_String( void ){
+    String* // FIXME, START HERE: ALLOCATE STRING 
+}
+
+
+
 ////////// BASIC MATH //////////////////////////////////////////////////////////////////////////////
-// FIXME: TOKENIZE  A MATH EXPRESSION
+
+enum TokenMode{
+    // Tokenizer flags
+    NO_CHAR, // Invalid
+    GOT_DGT, // Incoming digit      character
+    GOT_MNS, // Incoming minus      character
+    GOT_DEC, // Incoming decimal    character
+    GOT_SPC, // Incoming whitespace character
+    GOT_RES, // Incoming reserved   character
+    GOT_CHR, // All other characters
+    TKN_BGN,
+    TKN_UIN,
+};
+typedef enum TokenMode TokenMode;
+
+ubyte reserved_test( char c ){
+    // Is `c` a reserved character?
+    // WARNING: SLOW
+    if( c == '+' )  return 1;
+    if( c == '-' )  return 1;
+    return 0;
+}
+
+TokenMode character_test( char c ){
+    // Determine the type of character that was received
+    // WARNING: SLOW
+    if( isdigit( c ) /*-*/ )  return GOT_DGT;
+    if( c == '.' /*-----*/ )  return GOT_DEC;
+    if( c == '-' /*-----*/ )  return GOT_MNS;
+    if( isspace( c ) /*-*/ )  return GOT_SPC;
+    if( reserved_test( c ) )  return GOT_RES;
+    return GOT_CHR;
+}
+
+Queue* tokenize_math_expr( const char* exprStr, ulong Nchrs ){
+    // Return a queue of tokens relating to a math expression
+    Queue*    rtnQ  = make_Queue();
+    Queue*    token = make_Queue();
+    TokenMode mode = TKN_BGN;
+    TokenMode cTyp = NO_CHAR;
+    char /**/ c_i;
+    for( ulong i = 0; i < Nchrs; ++i ){
+        c_i  = exprStr[i];
+        cTyp = character_test( c_i );
+        switch( mode ){
+
+            /// State: Token Begin ///////////////
+            case TKN_BGN:
+                switch (cTyp){
+                    case GOT_DGT:
+                        push_back_Q( token )
+                        break;
+                    
+                    default:
+                        break;
+                }
+                break;
+            
+
+
+            default:
+                break;
+        }
+    }
+}
+
 // FIXME: EVALUTATE A MATH EXPRESSION
