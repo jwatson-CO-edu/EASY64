78f02e46ce57b04f71302f983ce74402d3316407
James Watson <james.watson-2@colorado.edu>
2025-04-04 16:49:07 -0600

    Begin high level lexing

--- cpp/CPluscal/00_chars.cpp
+++ cpp/CPluscal/00_chars.cpp
@@ -14,6 +14,14 @@ using std::map;
 using std::variant, std::get;
 #include <memory>
 using std::shared_ptr;
+#include <fstream>
+using std::ifstream;
+#include <iostream>
+using std::cout, std::endl, std::flush, std::cerr;
+#include <stdexcept>
+using std::runtime_error;
+#include <sstream>
+using std::stringstream, std::getline;
 
 /// Aliases ///
 typedef unsigned long  ulong;
@@ -45,6 +53,8 @@ const array<string,35> RESERVED = { /// Word Symbols ///
 const array<string,2> COMMENT_OPEN = { "{", "(*" };
 const array<string,2> COMMENT_CLOS = { "}", "*)" };
 
+const array<string,3> SECTION_NAMES = { "type", "var", "const" };
+
 
 
 ////////// BASIC LEXING ////////////////////////////////////////////////////////////////////////////
@@ -55,13 +65,16 @@ bool p_special( const string& q ){
     return false;
 }
 
+
 bool p_reserved( const string& q ){
     // Return True if `q` matches a symbol, otherwise return False
     for( const string& sym : RESERVED ){  if( q == sym ){  return true;  }  }
     return false;
 }
 
+
 bool p_symbol( const string& q ){  return p_special(q) || p_reserved(q);  }
+
 
 bool p_letter( const char& q ){
     // Return True if `q` matches a letter, otherwise return False
@@ -69,11 +82,13 @@ bool p_letter( const char& q ){
     return false;
 }
 
+
 bool p_digit( const char& q ){
     // Return True if `q` matches a digit, otherwise return False
     for( const char& dgt : DIGITS ){  if( q == dgt ){  return true;  }  }
     return false;
 }
+
 
 bool p_identifier( const string& q ){
     // Return True for strings that are: 1. Not symbols, 2. Begin with a letter, 3. Are composed of alphanumeric chars
@@ -85,6 +100,138 @@ bool p_identifier( const string& q ){
     }
     return true;
 }
+
+
+bool p_section_header( const string& q ){
+    // Return True if `q` matches a section name, otherwise return False
+    for( const string& nam : SECTION_NAMES ){  if( q == nam ){  return true;  }  }
+    return false;
+}
+
+
+string strip( const string& inputStr ){
+    // Remove whitespace from the beginning and end of a string
+    // Author: Kemin Zhou, https://stackoverflow.com/a/36169979
+    if( inputStr.empty() ){  return inputStr;  }
+    string::size_type i,j;
+    i = 0;
+    while( i < inputStr.size() && isspace( inputStr[i]) ){  ++i  };
+    if( i == inputStr.size() ){  return string();  } // empty string
+    j = inputStr.size() - 1;
+    while( isspace( inputStr[j] ) ){  --j;  }
+    return inputStr.substr( i, j-i+1 );
+}
+
+
+vstr tokenize( string expStr, char sepChr ){
+    // Return a vector of tokenized strings that a separated by whitespace within `expStr`
+    vstr   tokens;
+    char   c;
+    string cStr;
+    string token  = "";
+
+    // Helpers //
+    auto stow_token = [&]{  tokens.push_back( token );  token = "";  };
+    auto stow_char  = [&]{  tokens.push_back( string(1,c) );  };
+    auto cache_char = [&]{  token.push_back( c );  };
+
+    // 0. Apply the postfix hack
+    expStr.push_back( sepChr );
+
+    // 1. For every character in the string
+    for( char ch_i : expStr ){
+        // 2. Fetch character
+        c    = ch_i;
+        cStr = string(1,c);
+        // 3. Either add char to present token or create a new one
+        // A. Case Reserved
+        if( p_symbol( cStr ) ){  
+            // cout << "Reserved Token: " << find_reserved_token( cStr ) << endl;
+            if( token.size() > 0 ){  stow_token();  }
+            stow_char();  
+        // B. Case separator
+        }else if( c == sepChr ){
+            if(token.size() > 0){  stow_token();  }
+        // C. Case any other char
+        }else{  cache_char();  } 
+    }
+    // N. Return the vector of tokens
+    return tokens;
+}
+
+
+vector<vstr> segment_statements( vstr& tokens ){
+    // Separate a token vector into a vector of token vectors, each representing a statement
+    vector<vstr> statements;
+    vstr /*---*/ statement;
+    tokens.push_back( ";" ); // Terminator Hack
+    for( const string& token : tokens ){
+        if( token == ";" ){
+            if( statement.size() ){
+                statements.push_back( statement );
+                statement.clear();
+            }
+        }else{
+            statement.push_back( token );
+        }
+    }
+    return statements;
+}
+
+
+
+////////// FILE LEXING /////////////////////////////////////////////////////////////////////////////
+struct TextPortions{ public:
+    // Separates text of a file into portions so that we can treat each differently
+    string type;
+    string cnst;
+    string var;
+    string func;
+    string proc;
+    string prog;
+};
+
+
+TextPortions segment_source_file( string path ){
+    // Load sections of the program into the struct so that we can lex/interpret them
+    enum Section{ TYPE, CONST, VAR, COMMENT, OTHER };
+
+    ifstream     inputFile;
+    stringstream buffer;
+    vstr /*---*/ totLines;
+    string /*-*/ line, trimLine;
+    Section /**/ mode   = OTHER;
+    string /*-*/ errStr = "Could not open ";
+    
+    inputFile.open( path );
+    if( !inputFile.is_open() ){
+        cerr << "Error opening " << path << "!" << endl;
+        errStr += path;
+        inputFile.close();
+        throw runtime_error( errStr ); // Exit with an error code
+    }else{
+        buffer << inputFile.rdbuf(); // https://stackoverflow.com/a/2602258
+        while( getline( buffer, line, '\n' ) ){ // https://stackoverflow.com/a/13172579
+            totLines.push_back( line );
+        }
+        inputFile.close();
+    }
+    for( const string& line : totLines ){
+        trimLine = strip( line );
+        if( mode == COMMENT ){} // FIXME: CHECK FOR COMMENT END
+        if( p_section_header( trimLine ) ){
+            if( trimLine == "type" ){
+                mode = TYPE;
+            }else if( trimLine == "const" ){
+                mode = CONST;
+            }else if( trimLine == "var" ){
+                mode = VAR;
+            }
+        }
+    }
+}
+
+
 
 
 ////////// PRIMITIVE TYPES /////////////////////////////////////////////////////////////////////////
@@ -164,41 +311,6 @@ class ValStore{ public:
 };
 
 
-vstr tokenize( string expStr, char sepChr ){
-    // Return a vector of tokenized strings that a separated by whitespace within `expStr`
-    vstr   tokens;
-    char   c;
-    string cStr;
-    string token  = "";
-
-    // Helpers //
-    auto stow_token = [&]{  tokens.push_back( token );  token = "";  };
-    auto stow_char  = [&]{  tokens.push_back( string(1,c) );  };
-    auto cache_char = [&]{  token.push_back( c );  };
-
-    // 0. Apply the postfix hack
-    expStr.push_back( sepChr );
-
-    // 1. For every character in the string
-    for( char ch_i : expStr ){
-        // 2. Fetch character
-        c    = ch_i;
-        cStr = string(1,c);
-        // 3. Either add char to present token or create a new one
-        // A. Case Reserved
-        if( p_symbol( cStr ) ){  
-            // cout << "Reserved Token: " << find_reserved_token( cStr ) << endl;
-            if( token.size() > 0 ){  stow_token();  }
-            stow_char();  
-        // B. Case separator
-        }else if( c == sepChr ){
-            if(token.size() > 0){  stow_token();  }
-        // C. Case any other char
-        }else{  cache_char();  } 
-    }
-    // N. Return the vector of tokens
-    return tokens;
-}
 
 
 
