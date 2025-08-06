682baec8980d4ed999e29c6dc8ed710f0b4f9dcd
James Watson <james.watson-2@colorado.edu>
2025-08-06 15:53:58 -0600

    this is ill advised

---
+++ cpp/CPluscal/include/CPluscal.hpp
@@ -0,0 +1,87 @@
+#ifndef CPLUSCAL_HPP
+#define CPLUSCAL_HPP
+
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
+/// Imports ///
+#include <string>
+using std::string;
+#include <array>
+using std::array;
+#include <vector>
+using std::vector;
+
+
+/// Aliases ///
+typedef unsigned long  ulong;
+typedef long long /**/ llong;
+typedef unsigned char  ubyte;
+typedef vector<string> vstr;
+typedef vector<vstr>   vvstr; 
+
+
+
+////////// LANGUAGE CONSTANTS //////////////////////////////////////////////////////////////////////
+
+const array<char,52> LETTERS = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+};
+
+const array<char,10> DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+const array<string,24> SYMBOLS = { /// Special Symbols ///
+    "**", "+", "-", "*", "/", // Infix Operators
+    "=", "<", ">", "<>", "<=", ">=", ":=", "..", "<=", 
+    "[", "]", ".", ",", ":", ";", "^", "(", ")",
+    "'"
+};
+const size_t NUM_INFIX = 5;
+
+const array<string,35> RESERVED = { /// Word Symbols ///
+    "and", "array", "begin", "case", "const", "div", "do", "downto", "else", "end", "file", "for",
+    "function", "goto", "if", "in", "label", "mod", "nil", "not", "of", "or", "packed", "procedure",
+    "program", "record", "repeat", "set", "then", "to", "type", "until", "var", "while", "with"
+};
+
+const array<string,3> PRIM_TYPE_NAMES = { /// Primitive Type Names ///
+    "integer", "real", "Boolean",
+};
+
+const ubyte MIN_RES_SYMBOL_LEN = 1;
+const ubyte MAX_RES_SYMBOL_LEN = 9;
+
+const array<string,2> COMMENT_OPEN = { "{", "(*" };
+const array<string,2> COMMENT_CLOS = { "}", "*)" };
+
+const array<string,3> SECTION_NAMES = { "type", "var", "const" };
+
+const array<string,2> NL_INDICATORS = { "\r\n", "\n" };
+
+
+///// Internal Tokens /////////////////////////////////////////////////////
+const string TKN_NEWLINE = "<nl>";
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////// lexer.cpp ///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+bool p_special( const string& q ); // - Return True if `q` matches a symbol, otherwise return False
+bool p_infix_op( const string& q ); //- Return True if `q` matches an infix arithmetic operator, otherwise return False
+bool p_reserved( const string& q ); //- Return True if `q` matches a symbol, otherwise return False
+bool p_prim_type( const string& q ); // Return True if `q` matches a primtive type name, otherwise return False
+bool p_symbol( const string& q ); // -- Return True if either Special or Reserved
+bool p_letter( const char& q ); // ---- Return True if `q` matches a letter, otherwise return False
+bool p_digit( const char& q ); // ----- Return True if `q` matches a digit, otherwise return False
+// Return True for strings that are: 1. Not symbols, 2. Begin with a letter, 3. Are composed of alphanumeric chars
+bool p_identifier( const string& q ); 
+
+
+////////// LEXER ///////////////////////////////////////////////////////////////////////////////////
+class LexMachine{ public:
+
+};
+
+#endif
---
+++ cpp/CPluscal/lexer.cpp
@@ -0,0 +1,65 @@
+#include "include/CPluscal.hpp"
+
+
+
+////////// STRING PROCESSING ///////////////////////////////////////////////////////////////////////
+
+bool p_special( const string& q ){
+    // Return True if `q` matches a symbol, otherwise return False
+    for( const string& sym : SYMBOLS ){  if( q == sym ){  return true;  }  }
+    return false;
+}
+
+
+bool p_infix_op( const string& q ){
+    // Return True if `q` matches an infix arithmetic operator, otherwise return False
+    string sym;
+    for( size_t i = 0; i < NUM_INFIX; ++i ){
+        sym = SYMBOLS[i];
+        if( q == sym ){  return true;  }
+    }
+    return false;
+}
+
+
+bool p_reserved( const string& q ){
+    // Return True if `q` matches a symbol, otherwise return False
+    for( const string& sym : RESERVED ){  if( q == sym ){  return true;  }  }
+    return false;
+}
+
+
+bool p_prim_type( const string& q ){
+    // Return True if `q` matches a primtive type name, otherwise return False
+    for( const string& typ : PRIM_TYPE_NAMES ){  if( q == typ ){  return true;  }  }
+    return false;
+}
+
+
+bool p_symbol( const string& q ){  return p_special(q) || p_reserved(q);  }
+
+
+bool p_letter( const char& q ){
+    // Return True if `q` matches a letter, otherwise return False
+    for( const char& ltr : LETTERS ){  if( q == ltr ){  return true;  }  }
+    return false;
+}
+
+
+bool p_digit( const char& q ){
+    // Return True if `q` matches a digit, otherwise return False
+    for( const char& dgt : DIGITS ){  if( q == dgt ){  return true;  }  }
+    return false;
+}
+
+
+bool p_identifier( const string& q ){
+    // Return True for strings that are: 1. Not symbols, 2. Begin with a letter, 3. Are composed of alphanumeric chars
+    if( p_special(q) ){  return false;  }
+    if( p_reserved(q) ){  return false;  }
+    if( !p_letter( q[0] ) ){  return false;  }
+    for( const char& ch : q ){
+        if( (!p_letter( ch ))&&(!p_digit( ch )) ){  return false;  }
+    }
+    return true;
+}
